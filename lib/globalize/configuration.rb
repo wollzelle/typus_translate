dff815f11e75a17e8bb55f09de376974ac0be300
meleyal <william.meleyal@gmail.com>
2011-01-31 12:07:40 +0100

    use globalize configuration

--- lib/globalize/configuration.rb
+++ lib/globalize/configuration.rb
@@ -7,7 +7,7 @@ module Typus
       # Read configuration from <tt>config/typus/**/*.yml</tt>.
       def self.config!
         @@config = {}
-        file = Rails.root.join("config/typus/globalize.yml")
+        file = Rails.root.join("config/globalize.yml")
         raise "Error make sure the configuration file (#{file}) exists!" unless File.exists?(file)
         if data = YAML::load_file(file)
           @@config = data
