4859065e59d54205f3c85e0a3ac84c626c073ce7
Thomas König <tom@Tomks-Imac.local>
2012-01-17 19:01:25 +0100

    fixes duplication of translations if there are errors on the item

--- app/helpers/admin/translate_helper.rb
+++ app/helpers/admin/translate_helper.rb
@@ -1,35 +1,36 @@
 module Admin::TranslateHelper
-    
+
   class Translator
-    
+
     extend ActiveModel::Naming
     extend ActionView::Helpers
 
     ##
     # Class Methods
     #
-    
+
     @@locales = Typus::Translate::Configuration.config['locales'] rescue nil
     @@forms = {}
 
     cattr_accessor :forms, :locales
-    
+
     def self.setup_model(model)
       # make sure each locale exists
       # to setup the form fields/tabs for each locale
+      translations_empty = model.translations.empty?
       @@locales.each do |locale, name|
-        model.translations.find_or_initialize_by_locale(locale)
-      end 
-    end      
-    
-    def self.locales_json      
+        model.translations.find_or_initialize_by_locale(locale) if translations_empty
+      end
+    end
+
+    def self.locales_json
       raw @@locales.to_json rescue []
     end
-    
+
     def self.field_name_for(builder, attribute)
       "#{builder.object_name}[#{attribute.to_s.sub(/\?$/,"")}]"
     end
-      
+
     ##
     # Instance Methods
     #
@@ -50,7 +51,7 @@ module Admin::TranslateHelper
     end
 
     def field_type(field)
-      case @model.translations.columns_hash[@attribute].type 
+      case @model.translations.columns_hash[@attribute].type
       when :text
         field.text_area(@attribute)
       else
