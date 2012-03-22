9d913d9a0a69b2ce41164f6a35f367a031cf8456
meleyal <william.meleyal@gmail.com>
2012-03-22 14:58:17 +0100

    formatting

--- lib/translate/class_methods.rb
+++ lib/translate/class_methods.rb
@@ -1,19 +1,20 @@
 module Typus
   module Translate
+
     module ClassMethods
       def typus_translate(*args)
-        return if typus_translates?        
+        return if typus_translates?
         options = args.extract_options!
         translates *args
-        accepts_nested_attributes_for :translations  
+        accepts_nested_attributes_for :translations
         extend TemplateMethods
       end
-      
+
       def typus_translates?
         included_modules.include?(TemplateMethods)
-      end      
+      end
     end
-    
+
     module TemplateMethods
       def typus_template(attribute)
         if self.translated_attribute_names.include? attribute.to_sym
@@ -21,7 +22,8 @@ module Typus
         else
           super(attribute)
         end
-      end      
+      end
     end
+
   end
 end
